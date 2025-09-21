3d86ef185b108b875bb974f5d10e27c82b48d804
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-21 18:54:14 -0500

    fix(buttons): fix css style tailwind

--- src/components/buttons/button.jsx
+++ src/components/buttons/button.jsx
@@ -1,24 +1,26 @@
-const Button = ({ variant = "primary", children, ...props }) => {
+const Button = ({ variant = "primary", children }) => {
   const baseClasses = `
     font-bold px-6 py-2 rounded-full transition-all duration-150
   `;
 
   const variants = {
-    primary:
-      "bg-[#fd8b18] text-white shadow-cta active:bg-[#703800] active:shadow-none",
+      primary:
+      "bg-primary text-white shadow-cta active:bg-primary-darker active:shadow-none",
       accent:
-          "bg-[#3aaffe] text-white shadow-accent active:bg-accent-darker",
+          "bg-accent text-white shadow-accent active:bg-accent-darker",
       red:
-          "bg-[#da4447] text-white shadow-red active:bg-[#b12c2e] active:shadow-none",
-    white:
-      "bg-white text-black border-2 border-black shadow-regular active:bg-[#424242] active:text-white active:shadow-none",
+          "bg-red text-white shadow-red active:bg-red-darker active:shadow-none",
+      white:
+        "bg-white text-black border-2 border-black shadow-regular active:bg-gray-dark active:text-white active:shadow-none",
   };
 
   return (
-    <button className={`${baseClasses} ${variants[variant]}`} {...props}>
-      {children}
+    <button className={`${baseClasses} ${variants[variant]}`}>
+      {children} 
     </button>
   );
 };
 
 export default Button;
+
+//children = lo que va dentro del boton <Button>children</Button>
