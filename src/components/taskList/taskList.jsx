17d53824ed718a6b4f706c9086a5770ecf099677
Pangar03 <plopez0428@hotmail.com>
2025-09-22 13:35:46 -0500

    refa: applied format to taskList.jsx

--- src/components/taskList/taskList.jsx
+++ src/components/taskList/taskList.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const TaskList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return <div></div>;
+};
 
-export default TaskList
+export default TaskList;
