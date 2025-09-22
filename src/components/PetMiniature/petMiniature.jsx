399ac65ac38c212f26868d41dbbf6bc3b738f435
Isabella <angelcuartas18@gmail.com>
2025-09-21 21:36:53 -0500

    feat (petMiniature): agregué las constantes de mascota y color de fondo

---
+++ src/components/PetMiniature/petMiniature.jsx
@@ -0,0 +1,12 @@
+import naranja from '../../assets/naranja.svg'
+import frambuesa from '../../assets/rosa.svg'
+import mora from '../../assets/azul.svg'
+import fresa from '../../assets/verde.svg'
+
+
+const pets = [
+    { name: 'Naranja', img: naranja, bgColor: "#F9A74F" },
+    { name: 'Frambuesa', img: frambuesa, bgColor: "#F7B2C4" },
+    { name: 'Mora', img: mora, bgColor: "#E6CFFD" },
+    { name: 'Fresa', img: fresa, bgColor: "#F08080" },
+]
