0482e2366161af46d2ea6a0fdec3b480269f439b
Isabella <angelcuartas18@gmail.com>
2025-09-21 19:15:32 -0500

    cambié los emojis a una carpeta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Emojis from './components/MoodEmojis/emojis'
 
 function App() {
-  return <></>;
+  return <>
+    <Emojis />
+  </>;
 }
 
 export default App
--- src/components/MoodEmojis/emojis.jsx
+++ src/components/MoodEmojis/emojis.jsx
@@ -1,7 +1,10 @@
+
+
 const Emojis  = () => {
     return (
         <div className="mood-emojis">
             <p>Hola soy emojis is this working?</p>
+            <img src="" alt="Smiling Face with Heart-Eyes" width="50" height="50"/>
         </div>
     )
 }
