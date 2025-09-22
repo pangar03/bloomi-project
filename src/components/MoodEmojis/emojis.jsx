82e4f4eea6cf61e67dd3992ada4591aee79456bb
Isabella <angelcuartas18@gmail.com>
2025-09-21 19:12:00 -0500

    base de constante

--- src/components/MoodEmojis/emojis.jsx
+++ src/components/MoodEmojis/emojis.jsx
@@ -1,1 +1,9 @@
-const Emojis = 
+const Emojis  = () => {
+    return (
+        <div className="mood-emojis">
+            <p>Hola soy emojis is this working?</p>
+        </div>
+    )
+}
+
+export default Emojis
