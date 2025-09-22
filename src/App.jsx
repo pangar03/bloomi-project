5417445dc99185e95bfc2096e42cd321d62f90a3
Pangar03 <plopez0428@hotmail.com>
2025-09-22 13:06:21 -0500

    Revert "feat: add the button base component"
    This reverts commit 498ecdcd73505bc43da3de5817fb6ee83ca617d8.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import CheckButton from "./components/checkButton/checkButton";
 import Button from "./components/buttons/button";
