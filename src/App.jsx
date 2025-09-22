58719a54cf3ff1021624ea288d0c96e32d07ddbb
Isabella <angelcuartas18@gmail.com>
2025-09-21 20:42:49 -0500

    fix (app.jsx): arreglé el merge problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css'
 import Emojis from './components/MoodEmojis/emojis'
 
@@ -6,15 +5,6 @@ function App() {
   return <>
     <Emojis />
   </>;
-=======
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-    return <></>;
->>>>>>> 096eab84c24ce3e92b30dd9086936849b8a33399
 }
 
 export default App;
