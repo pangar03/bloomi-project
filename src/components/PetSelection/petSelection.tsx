9b849d1edd003c8124bdace1a00013e855415e83
Isabella <angelcuartas18@gmail.com>
2025-09-22 17:14:32 -0500

    feat(petSelection): created base for pets

--- src/components/PetSelection/petSelection.tsx
+++ src/components/PetSelection/petSelection.tsx
@@ -1,2 +1,9 @@
 import PetMiniature from "../PetMiniature/petMiniature";
 import Button from "../buttons/button";
+
+interface Pet {
+  name: string;
+  img: string;
+  bgColor: string;
+  locked: boolean;
+}
