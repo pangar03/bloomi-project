fff15100c69fac2efa603c33fffbe095ffeb4e0e
Pangar03 <plopez0428@hotmail.com>
2025-09-22 13:17:53 -0500

    Fix: clean the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,8 @@ import "./App.css";
 import "./index.css";
 import Button from "./components/buttons/button";
 
-
 function App() {
-    return <>
-        <Button variant="primary">Primary Button</Button>
-        <Button variant="accent">Accent Button</Button>
-        <Button variant="red">Red Button</Button>
-        <Button variant="white">White Button</Button>
-    </>
-        
+    return <></>;
 }
 
 export default App;
