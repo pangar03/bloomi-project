1d4a2fd7c41b986210c00484e2e632ca925330e7
Isabella <angelcuartas18@gmail.com>
2025-09-22 17:15:08 -0500

    feat(petSelection): created conts for the pet cards

--- src/components/PetSelection/petSelection.tsx
+++ src/components/PetSelection/petSelection.tsx
@@ -7,3 +7,10 @@ interface Pet {
   bgColor: string;
   locked: boolean;
 }
+
+const pets: Pet[] = [
+  { name: "Fresa", img: "/assets/fresa.svg", bgColor: "#F08080", locked: false },
+  { name: "Frambuesa", img: "/assets/rosa.svg", bgColor: "#F7B2C4", locked: false },
+  { name: "Mora", img: "/assets/azul.svg", bgColor: "#6A0DAD", locked: true },
+  { name: "Naranja", img: "/assets/naranja.svg", bgColor: "#F9A74F", locked: true },
+];
