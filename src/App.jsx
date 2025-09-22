255f7eb94c177fc52f7cb95e9b658113a7557a23
Isabella <angelcuartas18@gmail.com>
2025-09-21 21:49:48 -0500

    feat(appjsx): agregué el componente al jsx, todavía me da fallos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Emojis from './components/MoodEmojis/emojis'
+import PetMiniature from './components/PetMiniature/petMiniature';
 
 function App() {
   return <>
     <Emojis />
+    <PetMiniature />
   </>;
 }
 
