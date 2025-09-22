a9583574d5a8a617f107b48e7d16f7a4fe0d575a
Pangar03 <plopez0428@hotmail.com>
2025-09-22 13:12:22 -0500

    Revert "feat: first bottons (not the final botton)"
    This reverts commit 80a04689b35251c469cec708315de1cdb8f10050.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import CheckButton from "./components/checkButton/checkButton";
-import ButtonOrange from "./components/buttons/buttonOrange";
-import ButtonBlue from "./components/buttons/buttonBlue";
-import WhiteButton from "./components/buttons/whiteButton";
-import RedButton from "./components/buttons/redButton";
 
-
-    
 function App() {
-    return (
-        <>
-            <ButtonOrange />
-            <ButtonBlue />
-            <CheckButton />
-            <WhiteButton />
-            <RedButton />
-            </>
-    );
+    return <></>;
 }
 
 export default App;
