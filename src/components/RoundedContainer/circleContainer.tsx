429b81463f0bd0eb1f599e82b5544b7d27ce7f52
Pangar03 <plopez0428@hotmail.com>
2025-09-22 21:04:23 -0500

    feat: add rounded containers

---
+++ src/components/RoundedContainer/circleContainer.tsx
@@ -0,0 +1,24 @@
+type ContainerProps = React.HTMLAttributes<HTMLDivElement> & {
+    children: React.ReactNode;
+    variant?: keyof typeof styleVariants;
+};
+
+// Blue: Light blue background with medium blue border, on hover the border becomes darker blue; Use case: Default
+// Red: Light red background with medium red border, on hover the border becomes darker red; Use case: Delete, remove, error actions
+const styleVariants = {
+    blue: "bg-accent-lighter border-[3.5px] border-accent hover:border-accent-darker",
+    red: "bg-red-lighter border-[3.5px] border-red hover:border-red-darker",
+};
+
+// Way of using: <CircleContainer variant="blue|red">[IN HERE YOU PUT WHATEVER GOES INSIDE THE CONTAINER, LIKE ICON, H1, P, ETC]</CircleContainer>
+const CircleContainer = ({ children, variant, ...props }: ContainerProps) => {
+    return (
+        <div
+            className={`p-2 w-fit rounded-full flex items-center justify-center shadow-accent ${styleVariants[variant || "blue"]} ${props.className}`}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default CircleContainer;
