540cafd0d56c90d8c339d68cd9dcc27a4857e77d
Pangar03 <plopez0428@hotmail.com>
2025-09-23 23:21:10 -0500

    fix: svg usage error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
+import IconButton from "./components/buttons/iconButton";
 import "./index.css";
 
-
 function App() {
-    return <>
-        
-
-    </>;
+    return (
+        <>
+            <IconButton variant="ToothIcon" />
+        </>
+    );
 }
 
 export default App;
