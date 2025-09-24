89e2e4ff37977f1bb4a6968bfd9a77eebea97fc7
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-23 19:56:00 -0500

    delete(app.tsx): delete components for merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
 import "./index.css";
-import GoalCard from "./components/cards/goalCard/goalCard";
+
 
 function App() {
     return <>
-        <div className="p-4 space-y-4">
-            {/* Goal completado */}
-            <GoalCard variant="completed" goalName="Goal" reward={100} />
-            
-            {/* Goal en progreso */}
-            <GoalCard variant="progress" goalName="Goal" reward={100} progress="0/15" />
-        </div>
+        
+
     </>;
 }
 
