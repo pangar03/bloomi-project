3ace8f30f3d006ad67775bb77c93d856e5bb54ab
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-25 02:13:18 -0500

    delete(app): deleted the imports and components in

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import "./App.css";
 import "./index.css";
-import LoginForm
-    from "./components/LoginForm/loginForm";
- import Input from "./components/Input/input";
-
-
 
 function App() {
     return <>
-        <LoginForm />
-        
-        <Input/>
+
 
     </>;
 }
