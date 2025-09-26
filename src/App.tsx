a6fb0dfe03200e23fd74eefad4e725c54e5ebabe
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-26 15:08:21 -0500

    fix(app): the routes were fixed

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     return (
       <Router>
         <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
