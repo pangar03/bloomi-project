49097ee2f750b5bb1fce3a6818f5ab9d2315f161
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-26 22:11:40 -0500

    fix(input): fix input types and the structure (again)

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+
 import type { InputProps } from "../../types/InputTypes";
 
-const Input = ({ placeholder, type = "text",}: InputProps) => {
-  const [inputValue, setInputValue] = useState("");
 
+const Input = ({
+  placeholder,
+  type = "text",
+  inputValue,
+  setInputValue,
+  label,
+}: InputProps) => {
   return (
-    <input
-      type={type}
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)}
-      placeholder={placeholder}
-      className="border-2 border-blue-800 rounded-2xl px-4 py-3 text-blue-900 w-full"
-    />
+    <>
+      {label && (
+        <label className="mb-2 text-sm text-accent-darker pl-4">{label}</label>
+      )}
+      <input
+        type={type}
+        value={inputValue}
+        onChange={(e) => setInputValue?.(e.target.value)}
+        placeholder={placeholder}
+        className="border-2 border-blue-800 rounded-2xl px-4 py-3 text-blue-900 w-full"
+      />
+    </>
   );
 };
 
--- src/types/InputTypes.tsx
+++ src/types/InputTypes.tsx
@@ -1,12 +1,11 @@
-
 export type InputProps = {
   placeholder?: string;
   type?: string;
   label?: string;
   className?: string;
   value?: string;
+  inputValue?: string;
+  setInputValue?: (value: string) => void;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>, target: string) => void;
   required?: boolean;
 };
-
-
