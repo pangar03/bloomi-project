dd4fe0c4be8d7cbf0698fd10f8dec24ef8a772bd
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-26 23:57:45 -0500

    style(loginPage): insert image background (conflicts with the imports but functional)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,20 @@
-import LoginForm from '../components/LoginForm/loginForm'
-
+// src/pages/Login.tsx
+import BackGround from "../assets/background/background.png"; // ajusta ruta según archivo
+import LoginForm from "../components/LoginForm/loginForm";
 
 const Login = () => {
   return (
-    <div>
-      <LoginForm />
+    <div
+      className="min-h-screen w-full bg-cover bg-center flex flex-col"
+      style={{ backgroundImage: `url(${BackGround})` }}
+    >
+      <div className="mt-auto bg-white  radius-rounded: 32px shadow-lg p-10 w-full">
+        <div className="w-full max-w-md mx-auto">
+          <LoginForm />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
