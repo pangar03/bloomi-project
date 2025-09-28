9960f8de922db59ca2f411d3b7481e4669a10a4e
Isabella <angelcuartas18@gmail.com>
2025-09-27 19:27:45 -0500

    fix(app.tsx): connecting imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
+import Dashboard from "./pages/Dashboard/dashboard";
+import Store from "./pages/Store/store";
 
 
 
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/store" element={<Store />} />
         </Routes>
       </Router>
     );
