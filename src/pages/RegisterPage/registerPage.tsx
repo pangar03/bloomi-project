844cdc8451f9b76d1767de22f90b287f6e40ea94
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-28 04:01:49 -0500

    style(style): scale the logo

--- src/pages/RegisterPage/registerPage.tsx
+++ src/pages/RegisterPage/registerPage.tsx
@@ -15,7 +15,7 @@ const Register = () => {
     bg-[url(/src/assets/background/fondo.jpg)] md:bg-[url(/src/assets/background/brackground.jpg)]"
     >
       <div className="flex justify-center mt-20 mb-10">
-        <Logo variant="primary" />
+        <Logo variant="primary" className="scale-150" />
       </div>
       <div className="mt-auto bg-white rounded-t-4xl shadow-lg p-6 md:p-10 w-full min-h-[60vh] md:min-h-[70vh]">
         <div className="w-full max-w-md mx-auto">
--- src/pages/loginPage/login.tsx
+++ src/pages/loginPage/login.tsx
@@ -14,8 +14,8 @@ const Login = () => {
         className="min-h-screen w-full bg-cover bg-center flex flex-col
     bg-[url(/src/assets/background/fondo.jpg)] md:bg-[url(/src/assets/background/brackground.jpg)]"
       >
-        <div className="flex justify-center mt-20 mb-10 ">
-          <Logo variant="primary" />
+        <div className="flex justify-center mt-20 mb-10">
+          <Logo variant="primary" className="scale-150" />
         </div>
         <div className="mt-auto bg-white rounded-t-4xl shadow-lg p-6 md:p-10 w-full min-h-[60vh] md:min-h-[70vh]">
           <div className="w-full max-w-md mx-auto">
