519296851959cf3d68cd2c1bc5a640e15dc4e676
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-28 01:16:45 -0500

    style(registerForm): the size of the register change

--- src/components/registerForm/registerForm.tsx
+++ src/components/registerForm/registerForm.tsx
@@ -16,9 +16,9 @@ const RegisterForm = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col gap-4 w-full max-w-sm mx-auto"
+      className="flex flex-col gap-2 w-full max-w-sm mx-auto"
     >
-      <h1 className="text-3xl font-bold text-center">Register</h1>
+      <h1 className="text-xl font-bold text-center">Register</h1>
 
       <Input
         type="email"
