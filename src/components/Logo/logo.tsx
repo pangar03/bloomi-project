5fceec7a829d04523bec7f50d46d51a184f6415b
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-28 02:32:57 -0500

    fix(logo): the logo variantes are in use

--- src/components/Logo/logo.tsx
+++ src/components/Logo/logo.tsx
@@ -1,15 +1,20 @@
 import LogoSvg from "../../assets/logo/logo.svg";
 
 type LogoProps = {
+  variant?: "primary" | "white";
   className?: string;
 };
 
-const Logo = ({ className = "" }: LogoProps) => {
+const Logo = ({ variant = "primary", className = "" }: LogoProps) => {
   const baseClasses = "flex justify-center items-center transition-all duration-150";
+  const svgVariants = {
+    primary: "text-primary fill-current",
+    white: "text-white fill-current",
+  };
 
   return (
     <div className={`${baseClasses} ${className}`}>
-      <LogoSvg className="w-50 h-auto" aria-label="Bloomi Logo" />
+      <LogoSvg className={`w-50 h-auto ${svgVariants[variant]}`} aria-label="Bloomi Logo" />
     </div>
   );
 };
--- src/pages/loginPage/login.tsx
+++ src/pages/loginPage/login.tsx
@@ -12,12 +12,12 @@ const Login = () => {
     return (
       <div
         className="min-h-screen w-full bg-cover bg-center flex flex-col
-    bg-[url(/src/assets/background/background.png)]"
+    bg-[url(/src/assets/background/fondo.jpg)] md:bg-[url(/src/assets/background/brackground.jpg)]"
       >
         <div className="flex justify-center mt-20 mb-10 ">
-          <Logo variant="white" />
+          <Logo variant="primary" />
         </div>
-        <div className="mt-auto bg-white rounded-t-4xl shadow-lg p-10 w-full min-h-[70vh]">
+        <div className="mt-auto bg-white rounded-t-4xl shadow-lg p-6 md:p-10 w-full min-h-[60vh] md:min-h-[70vh]">
           <div className="w-full max-w-md mx-auto">
             <LoginForm />
           </div>
