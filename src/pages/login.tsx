2bac7365c38246c6f86d74bc48cb06089ca49909
Pangar03 <plopez0428@hotmail.com>
2025-09-27 23:07:33 -0500

    fix: manual resolve issues

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,5 @@
 // src/pages/Login.tsx
 import { useContext, useEffect } from "react";
-import BackGround from "../assets/background/background.png"; // ajusta ruta según archivo
 import LoginForm from "../components/LoginForm/loginForm";
 import { PageContext } from "../context/PageContext/PageContext";
 
@@ -11,11 +10,8 @@ const Login = () => {
     }, [currentPage, setCurrentPage]);
 
     return (
-        <div
-            className="min-h-screen w-full bg-cover bg-center flex flex-col"
-            style={{ backgroundImage: `url(${BackGround})` }}
-        >
-            <div className="mt-auto bg-white  radius-rounded: 32px shadow-lg p-10 w-full">
+        <div className="min-h-screen w-full bg-cover bg-center flex flex-col">
+            <div className="mt-auto bg-white  radius-rounded: 32px shadow-lg p-10 w-full bg-[url(/src/assets/background/background.png)]">
                 <div className="w-full max-w-md mx-auto">
                     <LoginForm />
                 </div>
