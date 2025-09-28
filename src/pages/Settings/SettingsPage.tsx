35b481c690d569f1ded3682e47bf69a75eab0b78
Pangar03 <plopez0428@hotmail.com>
2025-09-27 22:34:27 -0500

    fix: style fix and new settings route

--- src/pages/Settings/SettingsPage.tsx
+++ src/pages/Settings/SettingsPage.tsx
@@ -31,11 +31,13 @@ const SettingsPage = () => {
     return (
         <>
             <div
-                className={`${isUnlocked ? "hidden" : ""} pin-unlock-display flex flex-col gap-16 items-center mx-auto justify-center h-full bg-white`}
+                className={`${isUnlocked ? "hidden" : ""} pin-unlock-display flex flex-col gap-16 items-center mx-auto lg:justify-center justify-start h-full bg-white`}
             >
                 <div></div>
                 <div className="flex flex-col gap-4 items-center">
-                    <h1 className="text-l text-black">Enter your parent pin</h1>
+                    <h1 className="lg:text-l text-m font-bold text-black">
+                        Enter your parent pin
+                    </h1>
                     <h2 className="text-s text-black">
                         To access the <strong>settings view</strong>
                     </h2>
