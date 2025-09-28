4286cba14f90ab8d0eeb82a0aebc5da32157cbe7
Pangar03 <plopez0428@hotmail.com>
2025-09-27 22:30:53 -0500

    feat: added settings page

---
+++ src/pages/Settings/SettingsPage.tsx
@@ -0,0 +1,100 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../context/UserContext/UserContext";
+import NumPad from "../../components/NumPad/NumPad";
+import Button from "../../components/buttons/button";
+import { PageContext } from "../../context/PageContext/PageContext";
+
+const SettingsPage = () => {
+    const { user } = useContext(UserContext); //For now we will be using context
+    const [pinInputValue, setPinInputValue] = useState<string>("");
+    const [pinValue, setPinValue] = useState<string>("");
+    const [isUnlocked, setIsUnlocked] = useState<boolean>(false);
+    const { currentPage, setCurrentPage } = useContext(PageContext)!;
+
+    useEffect(() => {
+        if (currentPage !== "settings") setCurrentPage("settings");
+    }, []);
+
+    const handleUnlock = () => {
+        if (pinValue === user?.pin) {
+            setIsUnlocked(true);
+            alert("Access granted to settings");
+        }
+    };
+
+    useEffect(() => {
+        if (pinValue.length === 4) {
+            handleUnlock();
+        }
+    }, [pinValue]);
+
+    return (
+        <>
+            <div
+                className={`${isUnlocked ? "hidden" : ""} pin-unlock-display flex flex-col gap-16 items-center mx-auto justify-center h-full bg-white`}
+            >
+                <div></div>
+                <div className="flex flex-col gap-4 items-center">
+                    <h1 className="text-l text-black">Enter your parent pin</h1>
+                    <h2 className="text-s text-black">
+                        To access the <strong>settings view</strong>
+                    </h2>
+                    <NumPad
+                        input={pinInputValue}
+                        setInput={setPinInputValue}
+                        setPin={setPinValue}
+                    />
+                </div>
+            </div>
+            <div
+                className={`${isUnlocked ? "flex flex-col items-center justify-center    h-full gap-4 lg:w-[40%] w-[80%] mx-auto" : "hidden"} `}
+            >
+                <div>
+                    <div></div>
+                    <h1 className="text-l text-center font-bold text-black">
+                        Settings
+                    </h1>
+                </div>
+                <div className="flex flex-col gap-4 w-full">
+                    <Button
+                        variant="white"
+                        className="w-full"
+                        onClick={() => {}}
+                    >
+                        Edit Account
+                    </Button>
+                    <Button
+                        variant="white"
+                        className="w-full"
+                        onClick={() => {}}
+                    >
+                        Edit Habits
+                    </Button>
+                    <Button
+                        variant="accent"
+                        className="w-full"
+                        onClick={() => {}}
+                    >
+                        Weekly Habits Report
+                    </Button>
+                    <Button
+                        variant="accent"
+                        className="w-full"
+                        onClick={() => {}}
+                    >
+                        Mood Journal
+                    </Button>
+                </div>
+                <Button
+                    variant="red"
+                    className="w-full mt-16"
+                    onClick={() => {}}
+                >
+                    Log Out
+                </Button>
+            </div>
+        </>
+    );
+};
+
+export default SettingsPage;
