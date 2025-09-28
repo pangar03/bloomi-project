59ac7dac818065fa987ce1853948468715c1b2a5
SasaPlazas <saraplazasramirez@gmail.com>
2025-09-27 21:55:06 -0500

    feat(logo): logo svg problems

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,18 @@
 // src/pages/Login.tsx
-import BackGround from "../assets/background/background.png"; // ajusta ruta según archivo
+import Background from "../assets/background/brackground.jpg"
 import LoginForm from "../components/LoginForm/loginForm";
+import Logo from "../components/Logo/logo";
 
 const Login = () => {
   return (
     <div
-      className="min-h-screen w-full bg-cover bg-center flex flex-col"
-      style={{ backgroundImage: `url(${BackGround})` }}
+      className="bg-primary min-h-screen w-full bg-cover bg-center flex flex-col"
+      style={{ backgroundImage: `url(${Background})` }}
     >
-      <div className="mt-auto bg-white  radius-rounded: 32px shadow-lg p-10 w-full">
+      <div className="flex justify-center mt-20 mb-10">
+        <Logo variant="white" />
+      </div>
+      <div className="mt-auto bg-white rounded-t-4xl shadow-lg p-10 w-full">
         <div className="w-full max-w-md mx-auto">
           <LoginForm />
         </div>
